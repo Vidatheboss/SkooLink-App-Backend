ea7c46fc9d73def98c051ab5e1abfb3941c06d04
Dudu Missono <dudumissono@gmail.com>
2023-03-23 15:00:21 -0400

    updated user to users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const connection = require('./config/connection')
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/users')
 const studentRoute = require('./routes/student')
 const indexRoute = require('./routes')
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json())
-app.use('/user', userRoute)
+app.use('/users', userRoute)
 app.use('/students', studentRoute)
 app.use('/', indexRoute)
 
