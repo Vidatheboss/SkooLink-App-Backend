2364a4b5f3a27de7d08801542b129e5bdc84fc46
Your Name <you@example.com>
2023-03-26 11:13:31 -0400

    compose email part

--- routes/inbox.js
+++ routes/inbox.js
@@ -18,5 +18,26 @@ router.get('/messages/:id', (req, res) => {
     })
 })
 
+router.post('/compose', (req, res) =>{
+
+    let message= req.body
+
+    query = "INSERT INTO messages(sender_id, recipient_id, subject, message, date) VALUES(?, ?, ?, ?, CURDATE())"
+
+    connection.query(query, [message.senderEmail, message.recipientEmail, message.subjectLine, message.description], (err, results) => {
+        if(!err) {
+            if(results.affectedRows === 0){
+                return res.status(404).json({
+                    message: "Table does not exist"
+                })
+            }
+            return res.status(200).json({
+                message: "Message inserted Successfully"
+            })
+        } else {
+            return res.status(500).json(err)
+        }
+    })
+})
 
 module.exports = router
